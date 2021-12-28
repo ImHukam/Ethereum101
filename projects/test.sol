83981c9160448a2d4ddd8a17fdfc7e9e0a73a8f0
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2021-12-28 19:14:44 +0530

    Update test.sol

--- projects/test.sol
+++ projects/test.sol
@@ -1,4 +1,4 @@
 
 crowdfunding; https://ropsten.etherscan.io/address/0xf82f81b99f37ef55101a7641870edc7f27b1c652
 
-lottery : https://ropsten.etherscan.io/address/0x9f1ca25ec313134aad0e058c94a3a4079e66423e
+lottery : https://ropsten.etherscan.io/address/0x16d52172e080e1fa9a12652361289ba91faba3f3

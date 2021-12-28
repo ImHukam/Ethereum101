b7ed29d9ef16d815f46a3034b8f24cda47beee2e
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2021-12-28 17:58:01 +0530

    Delete Learning directory

--- Learning/hello.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.8.7;
-
-contract helloworld{
-
-    function hello() public pure returns(string memory){
-        return "Hello World"; 
-    }
-}
--- Learning/variables_datatypes.sol
+++
@@ -1,5 +0,0 @@
-int a=56; // signed integer 
-uint aa= 2; //unsigned integer - only >=0
-string b="hello";
-bool c= false;
-address d= 0xdfsdjjfsdfsfsdsdafewfwtrtrtw

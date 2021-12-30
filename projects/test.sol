93ba5dca86534b52c7fbc42c121a78dfb377b1f0
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2021-12-30 13:29:46 +0530

    Delete test.sol

--- projects/test.sol
+++
@@ -1,4 +0,0 @@
-
-crowdfunding : https://ropsten.etherscan.io/address/0x56d1d1351d15969ad7b4571e6c8564e16e926b2f#code
-
-lottery : https://ropsten.etherscan.io/address/0x16d52172e080e1fa9a12652361289ba91faba3f3#code
